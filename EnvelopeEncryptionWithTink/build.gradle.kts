67e074d42f44590aa19faa8ed5bb172650e0a535
Damien Burke <dburke@tzero.com>
2023-03-21 16:09:47 +0000

    Iterating

--- EnvelopeEncryptionWithTink/build.gradle.kts
+++ EnvelopeEncryptionWithTink/build.gradle.kts
@@ -1,7 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+val timestamp = System.currentTimeMillis()
+val containerVersion = timestamp
+
 plugins {
+    java
     kotlin("jvm") version "1.8.0"
+    `maven-publish`
+    id("com.diffplug.spotless") version "6.2.2"
 }
 
 group = "org.example"
@@ -9,9 +17,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.google.com/")
+    }
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/com.google.android/annotations
+    implementation("com.google.android:annotations:4.1.1.4")
+
+//    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("com.google.crypto.tink:tink:1.8.0")
     testImplementation(kotlin("test"))
 }
 
--- EnvelopeEncryptionWithTink/src/main/kotlin/com.damo/Driver.kt
+++ EnvelopeEncryptionWithTink/src/main/kotlin/com.damo/Driver.kt
@@ -1,4 +1,88 @@
 package com.damo
 
-class Driver {
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.BinaryKeysetReader
+import com.google.crypto.tink.CleartextKeysetHandle
+import com.google.crypto.tink.JsonKeysetWriter
+import com.google.crypto.tink.KeysetHandle
+import com.google.crypto.tink.aead.AeadConfig
+import com.google.crypto.tink.aead.AesGcmKeyManager
+import com.google.crypto.tink.aead.ChaCha20Poly1305KeyManager
+import java.io.ByteArrayOutputStream
+import java.util.Base64
+
+fun encryptKey(dek: KeysetHandle, kekAead: Aead): String =
+    ByteArrayOutputStream().use {
+        dek.write(JsonKeysetWriter.withOutputStream(it), kekAead)
+        String(it.toByteArray())
+    }
+
+
+fun main() {
+
+    AeadConfig.register()
+    println("Initiated Tink Aead")
+
+    val kek = KeysetHandle.generateNew(AesGcmKeyManager.aes128GcmTemplate())
+    val kekJson = keyJson(kek)
+    println("kekJson: $kekJson")
+
+    val kekAead = kek.getPrimitive(Aead::class.java)
+
+    var dek = KeysetHandle.generateNew(ChaCha20Poly1305KeyManager.chaCha20Poly1305Template())
+    val dekJson = keyJson(dek)
+    println("dekJson: $dekJson")
+
+    val encryptedDekJson = encryptKey(dek, kekAead)
+    println("DEK encrypted with KEK, will be stored with cipherText $encryptedDekJson ")
+
+    var dekAead = dek.getPrimitive(Aead::class.java)
+
+    val plainText = "Hello, Damo"
+
+    val cypherText = dekAead.encrypt(plainText.toByteArray(), null).base64EncodeToString()
+
+    println("cypherText: $cypherText")
+
+    val envelope = Envelope(encryptedDekJson, cypherText)
+
+    dekAead = null
+    dek = null
+
+    val decryptedFromEnvelope = decryptEnvelope(envelope, kekAead)
+    println("Decrypted From Envelope $decryptedFromEnvelope")
 }
+
+fun decryptEnvelope(envelope: Envelope, kekAead: Aead): String {
+
+    val dek = decryptKey(envelope.encryptedDekJson, kekAead)
+    println("DEK decrypted $dek ")
+    val dekAead = dek.getPrimitive(Aead::class.java)
+    return String(dekAead.decrypt(envelope.cypherText.base64Decode(), null))
+
+}
+
+fun decryptKey(encryptedKeyBase64: String, kekAead: Aead): KeysetHandle {
+    return KeysetHandle.read(
+        BinaryKeysetReader.withBytes(encryptedKeyBase64.base64Decode()),
+        kekAead
+    )
+}
+
+
+data class Envelope(val encryptedDekJson: String, val cypherText: String)
+
+fun keyJson(keysetHandle: KeysetHandle): String = ByteArrayOutputStream().use {
+    CleartextKeysetHandle.write(keysetHandle, JsonKeysetWriter.withOutputStream(it))
+    String(it.toByteArray())
+}
+
+fun ByteArray.base64EncodeToString(): String {
+    return Base64.getEncoder().encodeToString(this)
+}
+
+fun String.base64Decode(): ByteArray {
+    return Base64.getDecoder().decode(this)
+}
+
+
